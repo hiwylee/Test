cf8fd729fcb13919ed5fa7180844617bb2c47ed1
hiwylee <hiwylee@gmail.com>
2019-11-12 17:15:33 +0000

    phone-app baseline

---
+++ phone-app/src/App.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-lin
+
+import React, {Component} from 'react';
+import PhoneForm from './PhoneForm';
+
+
+class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
+  render() {
+    return (
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ phone-app/src/PhoneForm.js
@@ -0,0 +1,39 @@
+
+import React, {Component} from 'react';
+
+class PhoneForm extends Component {
+
+    state = {
+        name : '',
+        phone: ''
+    }
+
+    handleChange  = (e) => {
+        this.setState({
+            [e.target.name] : [e.target.value]
+        })
+    }
+
+    handleSubmit = (e) => {
+
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: ''
+        })        
+    }
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder='이름' value={this.state.name} name="name" onChange={this.handleChange } />                
+                <input placeholder='전화번호' value={this.state.phone} name="phone" onChange={this.handleChange } />
+                <div> {this.state.name}{this.state.phone}</div>
+                <div><button type="submit">등록</button></div>
+            </form>
+
+        );
+    }
+}
+
+export default PhoneForm;
