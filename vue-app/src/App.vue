b11fece8e80b624e75ed004ae0ed7c4f1c5ebd98
hiwylee <hiwylee@gmail.com>
2019-11-16 10:30:06 +0000

    m

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,17 +2,33 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <TodoItem 
+      v-for="item in groceryList"
+      v-bind:todo="item"
+      v-bind:key="item.id"
+    >ㅇ</TodoItem>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from './components/TodoItem.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    TodoItem
+  },
+  data()  {
+      return ({
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+      })
+  }  
 }
 </script>
 
---
+++ vue-app/src/components/TodoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="todo-item">
+    <li>{{todo.text}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: []
+  } ,
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  background-color: black;
+  color: aqua;
+  border: 'solid 1px gray';
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
